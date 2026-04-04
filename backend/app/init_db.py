508c4e325bd844440fd524c04a370c5232b4b90a
Adrian Osborne <162693958+AdrianOsborne@users.noreply.github.com>
2026-04-04 16:34:57 +1100

    Removed backend

--- backend/app/init_db.py
+++
@@ -1,5 +0,0 @@
-from app.db import Base, engine
-from app import models
-
-def init_db():
-    Base.metadata.create_all(bind=engine)
--- backend/sql/init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS docker_init_check (
-    id SERIAL PRIMARY KEY,
-    message TEXT NOT NULL
-);
-
-INSERT INTO docker_init_check (message)
-VALUES ('Docker DB init successful');
